d0149cf9206d75a9a93001ecd95a160950399904
KlauZ <125335098+DylanSantiagoRodriguez@users.noreply.github.com>
2025-03-12 08:10:52 -0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <main className="w-screen h-screen flex items-center justify-center bg-green-300">
+        <h1 className="text-green-950">Dylan Rodriguez</h1>
+      </main>
+
+  );
+}
